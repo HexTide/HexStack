d91e0ed8aa1a8460d5a2cf1a63ae964774ef2ca3
HexTide <50669105+HexTide@users.noreply.github.com>
2025-05-11 22:40:56 +0200

    Add runner for HexStack with dynamic path injection

---
+++ run.py
@@ -0,0 +1,16 @@
+import sys
+import os
+
+# Add HEX paths
+sys.path.append(os.path.abspath("../HexRoot"))
+sys.path.append(os.path.abspath("../HexLib"))
+sys.path.append(os.path.abspath("../HexDropper-Lite"))
+# sys.path.append(os.path.abspath("../HexClick"))
+# sys.path.append(os.path.abspath("../HexTrackr"))
+
+from hexstack.stack import HexStack
+from hexstack.config import STACK_SEQUENCE
+
+if __name__ == "__main__":
+    print("[HexStack] Running bot chain...")
+    HexStack(STACK_SEQUENCE).run()
